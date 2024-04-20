88473382109cb3bc8d5dec17ad86008c9032ac46
sumit021995 <kaku.saurav@gmail.com>
2024-04-20 21:33:43 +0530

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div name="contact" className="h-screen px-10 pb-20 sm:px-2 w-full bg-gradient-to-br from-[#124e66] to-[#124e66]">
+    <div name="contact" className="h-screen px-10 pb-10 sm:px-2 w-full bg-gradient-to-br from-[#124e66] to-[#124e66]">
       <div className="flex flex-col p-4 sm:justify-center max-w-screen-lg mx-auto text-white font-bold">
         <div className=" max-md:pt-[60px] md:pt-24">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
@@ -14,7 +14,7 @@ const Contact = () => {
                 <input type="text" name="name" placeholder="Enter your name:" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
                 <input type="text" name="email" placeholder="Enter your email:" className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
                 <textarea name="message" rows="6" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" placeholder="Enter your message"></textarea>
-                <button className="text-white bg-[#41BF61] px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Submit</button>
+                <button className="text-white bg-[#41BF61] px-6 py-3 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Submit</button>
             </form>
         </div>
       </div>
